0e0f2a8ab4b76abf95bdca268d7b163e1a8c207c
Ayub40 <ayubk4028@gmail.com>
2025-07-06 23:33:41 +0600

    aaa

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 const app: Application = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://library-management-system-liart-chi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
